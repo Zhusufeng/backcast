2d181e0909bee6dbcd8474c450e4759e7e5228cd
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-18 20:05:13 -0700

    Add fetch and parse funcs to videos.js

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,25 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  url: 'https://www.googleapis.com/youtube/v3/search',
+
+  search: function(query) {
+    this.fetch({
+      data: {
+        q: query, //accepts a string => query should already be a string
+        maxResults: 5, //retreive a limit of 5 results
+        key: window.YOUTUBE_API_KEY,
+        videoEmbeddable: true,
+        part: 'snippet',
+        type: 'video'
+
+      }
+    });
+  },
+
+  parse: function(data) {
+    return data.items;
+  }
 
 });
