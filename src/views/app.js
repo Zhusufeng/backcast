a76fb657362837a72671b30d42c4c8f127833d10
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-18 17:17:24 -0700

    Update order of app.js and assign placement

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,33 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    // console.log('initializing app');
+    this.videos = new Videos(window.exampleVideoData); //render a videos collection
+
+    // render app
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    //render videoplayerview
+    new VideoPlayerView({
+      model: this.videos.at(0),
+      collection: this.videos,
+      el: this.$('.player')
+    }).render();
+
+    //render searchview
+    new SearchView();
+
+    // render videolistview
+    new VideoListView({
+      collection: this.videos,
+      el: this.$('.list')
+    }).render();
+
     return this;
   },
 
