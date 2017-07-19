bcfb15dd38b9473fca412f9bbef9850a86923037
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-18 17:19:45 -0700

    Update VideoListEntryView

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,14 +1,5 @@
 var VideoListEntryView = Backbone.View.extend({
 
-  //target HTML videolistentry element
-  el: '.video-list-entry',
-
-  //initialize
-  initialize: function() {
-    //render
-    this.render();
-  },
-
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
